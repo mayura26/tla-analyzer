b7250dec82213a7bb8a0045c9d41b0c17e5a25c2
Mayura <km.vivekananda@gmail.com>
2025-06-29 17:32:53 -0400

    Updated middleware to allow monthly access to all

--- middleware.ts
+++ middleware.ts
@@ -7,13 +7,14 @@ export const config = {
      * - / (the root home page)
      * - /login (the login page)
      * - /weekly (the weekly trading data page)
+     * - /monthly (the monthly trading data page)
      * - /api (API routes)
      * - /_next/static (static files)
      * - /_next/image (image optimization files)
      * - /favicon.ico (favicon file)
      *
-     * This will protect all pages other than the home page, login page, and weekly page.
+     * This will protect all pages other than the home page, login page, weekly page, and monthly page.
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|login|weekly|$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|login|weekly|monthly|$).*)",
   ],
 } 
